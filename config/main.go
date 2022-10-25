72686c7c90dc42e3773e6b1750697b1997725b67
William Pring <william.pring@pm.me>
2022-10-24 20:31:46 -0400

    cleanup

--- config/main.go
+++ config/main.go
@@ -31,33 +31,3 @@ func GetConfig() (config Configurations, err error) {
 	err = viper.Unmarshal(&config)
 	return
 }
-
-// func GetConfig() {
-// 	// Set the file name of the configurations file
-// 	viper.SetConfigName("config")
-
-// 	// Set the path to look for the configurations file
-// 	viper.AddConfigPath(".")
-
-// 	// // Enable VIPER to read Environment Variables
-// 	// viper.AutomaticEnv()
-
-// 	viper.SetConfigType("yml")
-// 	var configuration Configurations
-
-// 	if err := viper.ReadInConfig(); err != nil {
-// 		fmt.Printf("Error reading config file, %s", err)
-// 	}
-
-// 	err := viper.Unmarshal(&configuration)
-// 	if err != nil {
-// 		fmt.Printf("Unable to decode into struct, %v", err)
-// 	}
-
-// 	// Reading variables using the model
-// 	fmt.Println("Reading variables using the model..")
-// 	fmt.Println("Database is\t", configuration.Stmp.Host)
-// 	fmt.Println("Port is\t\t", configuration.Stmp.Pass)
-// 	fmt.Println("EXAMPLE_PATH is\t", configuration.Stmp.Port)
-// 	fmt.Println("EXAMPLE_VAR is\t", configuration.Stmp.User)
-// }
